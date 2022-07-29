a4c3ac4d80c911020bb888480dfae4cc017c498e
libero <liberopassadorneto@gmail.com>
2022-07-28 21:49:44 -0400

    criando-controller

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,5 +1,4 @@
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
+import { inject, injectable } from "tsyringe";
 
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
@@ -12,9 +11,12 @@ interface IRequest {
     expected_return_date: Date;
 }
 
+@injectable()
 class CreateRentalUseCase {
     constructor(
+        @inject("RentalsRepository")
         private rentalsRepository: IRentalsRepository,
+        @inject("DayJsDateProvider")
         private dateProvider: IDateProvider
     ) {}
 
