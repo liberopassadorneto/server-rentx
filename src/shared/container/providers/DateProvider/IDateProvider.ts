c07bc6f206bd1bab572893bb83a6593a7d23319c
libero <liberopneto@gmail.com>
2022-09-02 11:52:14 -0400

    caso-de-uso-de-reset-de-senha

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -5,6 +5,7 @@ interface IDateProvider {
     compareInDays(end_date: Date, start_date: Date): number;
     addDays(days: number): Date;
     addHours(hours: number): Date;
+    compareIfBefore(start_date: Date, end_date: Date): boolean;
 }
 
 export { IDateProvider };
