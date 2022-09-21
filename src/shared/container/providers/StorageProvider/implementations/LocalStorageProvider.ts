17df889344ac40f0916f31afb8acfea7765ae86b
libero <liberopneto@gmail.com>
2022-09-21 08:01:12 -0400

    refactor: avatar upload

---
+++ src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+import { resolve } from "path";
+
+import upload from "@config/upload";
+
+import { IStorageProvider } from "../IStorageProvider";
+
+class LocalStorageProvider implements IStorageProvider {
+    async save(file: string, folder: string): Promise<string> {
+        await fs.promises.rename(
+            resolve(upload.tmpFolder, file),
+            resolve(`${upload.tmpFolder}/${folder}`, file)
+        );
+
+        return file;
+    }
+
+    async delete(file: string, folder: string): Promise<void> {
+        const fileName = resolve(`${upload.tmpFolder}/${folder}`, file);
+
+        try {
+            // stat -> verifica se o arquivo existe (ou não) no diretório pesquisado
+            await fs.promises.stat(fileName);
+        } catch {
+            return;
+        }
+        // unlink -> deleta o arquivo
+        await fs.promises.unlink(fileName);
+    }
+}
+
+export { LocalStorageProvider };
